b51dd51ad6356b1b4265e6a70ca68fd3277d6fce
Tiana Clemons <tiana.clemons@duke.edu>
2024-06-24 02:04:32 -0400

    completed styling, contact and home page

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://tiananc.github.io',
-  base: 'my-repo',
+  base: 'cyberland',
 });
